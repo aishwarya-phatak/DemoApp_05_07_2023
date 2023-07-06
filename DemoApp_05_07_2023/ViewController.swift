85637f5b57f837aca31a9d2673354a56c51ddd24
aishwarya <ashp17star@gmail.com>
2023-07-06 16:12:57 +0530

    Initial Commit

---
+++ DemoApp_05_07_2023/ViewController.swift
@@ -0,0 +1,74 @@
+//
+//  ViewController.swift
+//  DemoApp_05_07_2023
+//
+//  Created by Vishal Jagtap on 05/07/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var firstLabel: UILabel!
+    @IBOutlet weak var secondLabel: UILabel!
+    @IBOutlet weak var thirdLabel: UILabel!
+    @IBOutlet weak var userdataTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func changeColourButton(_ sender: Any) {
+//        print(sender.currentTitle)
+//        print(sender.backgroundColor)
+//        print(sender.buttonType)
+        
+        guard let textEnteredByuser = userdataTextField.text else {
+            return
+        }
+
+        implementColourChange(text: textEnteredByuser)      //function call
+        /*switch textEnteredByuser{
+            case "one" :
+                firstLabel.backgroundColor = .red
+                secondLabel.backgroundColor = .brown
+                thirdLabel.backgroundColor = .brown
+            
+            case "two" :
+                firstLabel.backgroundColor = .brown
+                secondLabel.backgroundColor = .red
+                thirdLabel.backgroundColor = .brown
+            
+            case "three" :
+                firstLabel.backgroundColor = .brown
+                secondLabel.backgroundColor = .brown
+                thirdLabel.backgroundColor = .red
+            
+            default:
+                firstLabel.backgroundColor = .lightGray
+                secondLabel.backgroundColor = .lightGray
+                thirdLabel.backgroundColor = .lightGray
+        }*/
+    }
+
+    func implementColourChange(text : String){
+        
+        if(text  == "one"){
+            firstLabel.backgroundColor = .orange
+            secondLabel.backgroundColor = .brown
+            thirdLabel.backgroundColor = .brown
+        } else if(text == "two"){
+            firstLabel.backgroundColor = .brown
+            secondLabel.backgroundColor = .orange
+            thirdLabel.backgroundColor = .brown
+        } else if(text == "three"){
+            firstLabel.backgroundColor = .brown
+            secondLabel.backgroundColor = .brown
+            thirdLabel.backgroundColor = .orange
+        } else {
+            firstLabel.backgroundColor = .lightGray
+            secondLabel.backgroundColor = .lightGray
+            thirdLabel.backgroundColor = .lightGray
+        }
+    }
+}
